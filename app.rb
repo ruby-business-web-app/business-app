ccf6cd3785eceadfda9ac8f643b7669029266620
Bridget Keehan <ba.keehan@gmail.com>
2017-05-12 18:00:46 -0400

    content slides over hero image closes #11

--- app.rb
+++ app.rb
@@ -7,20 +7,24 @@ end
 
 get '/photopage' do 
 	@heading = "hero"
+	@page_type = "withsmall"
 	erb :photopage
 end
 
 get '/' do
 	@heading = "index-hero"
+	@page_type = "withbig"
 	erb :index
 end
 
 get '/menu' do
 	@heading = "hero"
+	@page_type = "withsmall"
 	erb :menu
 end
 
 get '/contact' do
 	@heading = "hero"
+	@page_type = "withsmall"
 	erb :contact
 end
