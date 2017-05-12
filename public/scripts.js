a8a0e2d8c753ff9ae7e7bedcc495993f9fabca3a
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-05-12 16:44:58 -0400

    menu page framework set up. closes #6

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,17 @@
-menuPhotos = document.getElementsByClassName("menu-item");
+menuItemContainers = document.getElementsByClassName("menu-item-container");
 
 
-[].forEach.call(menuPhotos, function(photo){
-	photo.addEventListener("hover", function(){
-		photo.classList.add("menu-item-enlarged");
+// [].forEach.call(menuPhotos, function(photo){
+// 	photo.addEventListener("hover", function(){
+// 		photo.classList.add("menu-item-enlarged");
+// 	})
+// })
+
+Array.prototype.forEach.call(menuItemContainers, function(photo){
+	photo.addEventListener("mouseenter", function(){
+		photo.classList.toggle("menu-item-overlay");
+	})
+	photo.addEventListener("mouseleave", function(){
+		photo.classList.toggle("menu-item-overlay");
 	})
 })
