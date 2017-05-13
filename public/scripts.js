fa53c882025f4db5dfa874a5089d5346d05e0f0e
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-05-13 14:16:22 -0400

    trying to figure out text on top of overlay

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,31 @@
 menuItemContainers = document.getElementsByClassName("menu-item-container");
+menuItems = document.getElementsByClassName("menu-item");
+menuItemTitles = document.getElementsByClassName("menu-item-title");
+menuItemTexts = document.getElementsByClassName("menu-item-text");
 
+addTextEventListeners();
 
-// [].forEach.call(menuPhotos, function(photo){
-// 	photo.addEventListener("hover", function(){
-// 		photo.classList.add("menu-item-enlarged");
-// 	})
-// })
-
-Array.prototype.forEach.call(menuItemContainers, function(photo){
+Array.prototype.forEach.call(menuItems, function(photo){
 	photo.addEventListener("mouseenter", function(){
-		photo.classList.add("menu-item-overlay");
+		photo.classList.remove("menu-item-overlay");
+		photo.classList.add("menu-item-shadow");
 	})
 	photo.addEventListener("mouseleave", function(){
-		photo.classList.remove("menu-item-overlay");
+		photo.classList.add("menu-item-overlay");
+		photo.classList.remove("menu-item-shadow");
 	})
 })
+
+function addTextEventListeners() {
+	Array.prototype.forEach.call(menuItemTitles, function(title){
+		title.addEventListener("mouseenter", function(){
+			setTimeout(function(){title.style.display = "none"}, 1000);
+		})
+	})
+
+	Array.prototype.forEach.call(menuItemTexts, function(text){
+		text.addEventListener("mouseleave", function(){
+			setTimeout(function(){text.style.display = "block"}, 1000);
+		})
+	})
+}
