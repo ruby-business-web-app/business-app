fc367971e3ec8caf131fda14c155e1c5a5469366
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-05-14 15:43:02 -0400

    added menu sections and internal links, closes #13

--- public/scripts.js
+++ public/scripts.js
@@ -1,24 +1,15 @@
-// menuItemContainers = document.getElementsByClassName("menu-item-container");
-// menuItemTitles = document.getElementsByClassName("menu-item-title");
-// menuItemTexts = document.getElementsByClassName("menu-item-text");
 menuItems = document.getElementsByClassName("menu-item");
 menuItemTextDivs = document.getElementsByClassName("menu-item-text-div");
 
 
-Array.prototype.forEach.call(menuItems, function(photo){
+Array.prototype.forEach.call(menuItems, function(photo, index){
 	photo.addEventListener("mouseenter", function(){
 		photo.classList.add("menu-item-shadow");
+		setTimeout(function(){menuItemTextDivs[index].style.display = "none"}, 800);
+
 	})
 	photo.addEventListener("mouseleave", function(){
 		photo.classList.remove("menu-item-shadow");
-	})
-})
-
-Array.prototype.forEach.call(menuItems, function(item, index){
-	menuItemTextDivs[index].addEventListener("mouseenter", function(){
-		setTimeout(function(){menuItemTextDivs[index].style.display = "none"}, 800);
-	});
-	item.addEventListener("mouseleave", function(){
 		setTimeout(function(){ menuItemTextDivs[index].style.display = "block"}, 800);
-	});
+	})
 })
