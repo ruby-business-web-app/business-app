89b6fd612a221cb75f0b88ece630bff80ffb6619
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-05-15 13:33:47 -0400

    progress on contact form

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'sendgrid-ruby'
+include SendGrid
 
 
 get '/layout' do
@@ -27,4 +29,18 @@ get '/contact' do
 	@heading = "hero"
 	@page_type = "withsmall"
 	erb :contact
+end
+
+post '/contact' do
+	from = Email.new(email: params[:from_email])
+	to = Email.new(email: params[:to_email])
+	subject = params[:subject]
+	content = Content.new(type: 'text/plain', value: params[:content])
+	mail = Mail.new(from, subject, to, content)
+
+	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	puts response.status_code
+	puts response.body
+	puts response.headers
 end
