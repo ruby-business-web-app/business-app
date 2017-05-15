b9f9f00d3abdbd611a05aadd84ad08ded8f90e0a
Matt Lomet <mattlomet@live.com>
2017-05-15 13:33:19 -0400

    contact post

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 
 
+require 'sendgrid-ruby'
+
+include SendGrid
+
+
 get '/layout' do
 	erb :layout
 end
@@ -28,3 +33,18 @@ get '/contact' do
 	@page_type = "withsmall"
 	erb :contact
 end
+
+post '/contact' do
+	from = Email.new(email: 'test@example.com')
+	to = Email.new(email: 'test@example.com')
+	subject = 'Sending with SendGrid is Fun'
+	content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+	mail = Mail.new(from, subject, to, content)
+
+	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	puts response.status_code
+	puts response.body
+	puts response.headers
+end
+
