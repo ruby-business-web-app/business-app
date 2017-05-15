a1827462c2dc9d8cdd6bf40372f98c65073aa0ee
Matt Lomet <mattlomet@live.com>
2017-05-15 14:57:21 -0400

    email page working needs styling

--- app.rb
+++ app.rb
@@ -38,15 +38,20 @@ end
 
 post '/contact' do
 
-	from = Email.new(email: params[:from_email])
-	to = Email.new(email: params[:to_email])
-	subject = params[:subject]
-	content = Content.new(type: 'text/plain', value: params[:content])
-	mail = Mail.new(from, subject, to, content)
+	@from = Email.new(email: params[:from_email])
+	@to = Email.new(email: params[:to_email])
+	@subject = params[:subject]
+	@content = Content.new(type: 'text/plain', value: params[:content])
+	@mail = Mail.new(@from, @subject, @to, @content)
 
+	
 	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	response = sg.client.mail._('send').post(request_body: @mail.to_json)
 	puts response.status_code
 	puts response.body
 	puts response.headers
+
+	@heading = "hero"
+	@page_type = "withsmall"
+	erb :contact
 end
